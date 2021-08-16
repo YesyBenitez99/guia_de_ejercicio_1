0c7a0ff51fbe8538a7ed8b40e1f1d240abd11e1a
YesyBenitez99 <47905130+YesyBenitez99@users.noreply.github.com>
2021-08-15 21:26:14 -0300

    Add files via upload

---
+++ ejercicio 5.py
@@ -0,0 +1,10 @@
+# ejercicio 5
+#Rudy Daniel sanchez Martinez SMTS008619
+
+a=int(input("ingrese un numero: "))
+b=int(input("ingrese un numero: "))
+
+if a>b:
+    print ('el mayor es',a)
+if b>a:
+    print ('el mayor es',b)
