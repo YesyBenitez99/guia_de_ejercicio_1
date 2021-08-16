131a1f0bdc8be1cf92a518962fad59874613004b
YesyBenitez99 <47905130+YesyBenitez99@users.noreply.github.com>
2021-08-15 21:25:39 -0300

    Add files via upload

---
+++ ejercicio 3.py
@@ -0,0 +1,11 @@
+# Yesy Emilia Benitez Rios SMTS936120
+#ejercicio 3
+
+
+Fahrenheit = float(input("ingrese temperatura en Fahrenheit:"))
+
+Celsius = (5/9) * (Fahrenheit-32)
+print("El resultado en Celsius es", Celsius)
+
+
+
