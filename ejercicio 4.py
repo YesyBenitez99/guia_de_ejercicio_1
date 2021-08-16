3ef5647bfeacb2955f4d4c34995fe0501ca1bc63
YesyBenitez99 <47905130+YesyBenitez99@users.noreply.github.com>
2021-08-15 21:25:57 -0300

    Add files via upload

---
+++ ejercicio 4.py
@@ -0,0 +1,13 @@
+#ejercicio 4
+#Manuel Enrique Benavides Batres
+
+key = " ugb"
+
+nombre = input (" Ingrese su nombre: ")
+password = input ("Ingrese su contraseña: ")
+
+if key == password.lower():
+    print("La contraseña coincide")
+
+else:
+    print("La contraseña no coincide")
